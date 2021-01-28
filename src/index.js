eb3cd3da1b2f863d7ca9811ca07cd36f212e5902
Tom Coemans <tom.coemans@gmail.com>
2021-01-28 18:29:54 +0100

    ES export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-export * from '../dist/vl-doormat-all.src.js';
+import {VlDoormat} from '../dist/vl-doormat.src.js';
+import {VlDoormatContent} from '../dist/vl-doormat-content.src.js';
+import {VlDoormatTitle} from '../dist/vl-doormat-title.src.js';
+import {VlDoormatText} from '../dist/vl-doormat-text.src.js';
+import {VlDoormatImage} from '../dist/vl-doormat-image.src.js';
+import {VlDoormatGraphicWrapper} from '../dist/vl-doormat-graphic-wrapper.src.js';
+
+export {
+  VlDoormat,
+  VlDoormatContent,
+  VlDoormatTitle,
+  VlDoormatText,
+  VlDoormatImage,
+  VlDoormatGraphicWrapper,
+};
