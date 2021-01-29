5d67e1f9dc4c731742f86779b55dd058d56a89b8
Tom Coemans <tom.coemans@gmail.com>
2021-01-29 10:13:17 +0100

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-import {VlDoormat} from '../dist/vl-doormat.src.js';
-import {VlDoormatContent} from '../dist/vl-doormat-content.src.js';
-import {VlDoormatTitle} from '../dist/vl-doormat-title.src.js';
-import {VlDoormatText} from '../dist/vl-doormat-text.src.js';
-import {VlDoormatImage} from '../dist/vl-doormat-image.src.js';
-import {VlDoormatGraphicWrapper} from '../dist/vl-doormat-graphic-wrapper.src.js';
-
-export {
-  VlDoormat,
-  VlDoormatContent,
-  VlDoormatTitle,
-  VlDoormatText,
-  VlDoormatImage,
-  VlDoormatGraphicWrapper,
-};
+export * from '../dist/vl-doormat.src.js';
